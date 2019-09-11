55a4205fd3e5f7450793286f423f884fb6a08a07
Anurag Regmi <anuragregmi@protonmail.com>
2019-09-11 22:44:26 +0545

    Add url in publication

--- nefosta/models/publication.py
+++ nefosta/models/publication.py
@@ -6,6 +6,19 @@ from nefosta.models.abstract import BaseModel
 class Publication(BaseModel):
     title = models.CharField(max_length=150, unique=True)
     content = models.TextField()
+    _url = models.URLField("URL", blank=True)
+    file = models.FileField(null=True, blank=True)
+
+    def save(self):
+        if not(self._url or self.file):
+            raise ValidationError("Either `url` or `file` must be set.")
+        super().save()
+
+    @property
+    def url(self):
+        if self.file:
+            return self.file.url
+        return self._url
 
     def __str__(self):
         return self.title
